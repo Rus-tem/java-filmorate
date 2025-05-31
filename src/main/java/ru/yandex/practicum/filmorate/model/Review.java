dc8b65b9f87b864595a0f6fbd9424e3d08d28964
Evstafev <tehnol25@yandex.ru>
2025-05-31 23:11:18 +0700

    ТЗ 13 add-reviews (отзывы)

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+
+
+@Data
+public class Review {
+
+    private Long reviewId;
+
+    private String content;
+
+    private Boolean isPositive;
+
+    private Long userId;
+
+    private Long filmId;
+
+    @Value("0")
+    private Long useful;
+
+}
